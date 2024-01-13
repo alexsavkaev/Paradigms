b9af5d128fb6d1b8aa3eea61a6d75afb19efaa64
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2024-01-13 17:02:27 +0300

    added homework
    + checked out Codium tests

--- homework.py
+++ homework.py
@@ -1,7 +1,11 @@
 def sort_list_imperative(numbers):
     n = len(numbers)
     for i in range(n):
-        for j in range(0, n-i-1):
-            if numbers[j] > numbers[j+1]:
-                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
     return numbers
+
+
+def sort_list_declarative(numbers):
+    return sorted(numbers)
